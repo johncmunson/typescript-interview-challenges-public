1dd1b6a521700de2116ef56e83b3ea2c7f5df704
John Munson <19480078+johncmunson@users.noreply.github.com>
2024-02-05 15:15:17 -0600

    updates

--- index.ts
+++ index.ts
@@ -1,18 +1,24 @@
 // 1. Write a function called `compact` that removes all falsy values from an array
 export const compact = () => {}
 
-// 2. Write function called `sumValues` to calculate the sum of all the number in an array
+// 2. Write a function called `sumValues` to calculate the sum of all the number in an array
+export const sumValues = () => {}
 
 // 3. Write a function called `addOrRemove` that adds an item to an array if it doesn't already exist, and removes it if not.
 //    The array can be heterogeneous, but only contain primitive values.
+export const addOrRemove = () => {}
 
 // 4. Write a function called `chunk` that accepts an array and splits it into groups of length `size`. If the array can't be split evenly, the final chunk will be the remaining elements.
+export const chunk = () => {}
 
 // 5. Write a function called `arrayDifference` that returns all the values from the first array that are not present in any of the other provided arrays
+export const arrayDifference = () => {}
 
 // 6. Write a function called `isPalindrome` that returns true if a string is a palindrome, and false otherwise
+export const isPalindrome = () => {}
 
 // 7. Write a function called `getAllKeys` that returns all the keys of a nested object
+export const getAllKeys = () => {}
 
 // 8. Write a class called `Calculator` with the following requirements...
 //      - may optionally be initialized with a starting value, otherwise it starts with zero in the register
@@ -24,7 +30,10 @@ export const compact = () => {}
 //      const myCalc = new Calculator(3)
 //      const result = myCalc.add(2).multiply(10).calculate()
 //      console.log(result) //=> 50
+export class Calculator {}
 
 // 9. Write a function called `sleep` that awaits a given number of milliseconds before resolving
+export const sleep = () => {}
 
 // 10. Write a function called `memoize` that memoizes a function that accepts a variable number of arguments and returns it
+export const memoize = () => {}
